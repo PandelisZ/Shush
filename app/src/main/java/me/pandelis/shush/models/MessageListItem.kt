6f835c2a1204f4a0187399b7a2af758b457538e2
Pandelis Zembashis <info@pandelis.me>
2019-01-12 16:34:32 +0000

    Views

---
+++ app/src/main/java/me/pandelis/shush/models/MessageListItem.kt
@@ -0,0 +1,9 @@
+package me.pandelis.shush.models
+
+class Product(
+    val id: Int,
+    val sender: String,
+    val lastMessage: String,
+    val lastMessageRecievedTime: String,
+    val image: Int
+)
